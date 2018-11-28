dcbf96a8e7d899af6e0789834f18141e7c421bac
Tomas Muñoz Testón <tomini18@hotmail.com>
2018-11-28 23:45:48 +0100

    Config toast to controller input in login panel.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -5,6 +5,8 @@ import { Router } from '@angular/router';
 
 import { ToastrService } from 'ngx-toastr';
 
+import { ToastErrorSettings } from '../../shared/config/configToastError.model';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -13,24 +15,36 @@ import { ToastrService } from 'ngx-toastr';
 export class LoginComponent {
   constructor(private loginService: LoginService, private router: Router, private toastr: ToastrService) { }
 
+  errorUser = false;
+  errorPassword = false;
+
   onLogin(form: NgForm) {
     const usuario = form.value.usuario;
     const password = form.value.password;
 
-    this.loginService.doLogin(usuario, password).subscribe(
-      resp => {
-        if (resp) {
-          console.log(resp.headers.get('Authorization'));
-          sessionStorage.setItem('token', resp.headers.get('Authorization'));
-          this.loginService.changeMode();
-          // this.toastr.success('Hello world!', 'Toastr fun!');
-          this.router.navigate(['/']);
-        } else {
-          console.log('Error');
+    if (usuario.length === 0 || password.length === 0) {
+      this.errorInputs(true);
+      this.toastr.error('Introduce usuario y contraseña', 'Error', ToastErrorSettings.TOAST_ERROR_SETINGS);
+    } else {
+      this.loginService.doLogin(usuario, password).subscribe(
+        resp => {
+          if (resp) {
+            sessionStorage.setItem('token', resp.headers.get('Authorization'));
+            this.loginService.changeMode();
+            this.router.navigate(['/']);
+          } else {
+            this.toastr.error('Tu usuario y/o contraseña son incorrectos.', 'Error', ToastErrorSettings.TOAST_ERROR_SETINGS);
+          }
+        },
+        error => {
+          this.toastr.error('Tu usuario y/o contraseña son incorrectos.', 'Error', ToastErrorSettings.TOAST_ERROR_SETINGS);
         }
-      },
-      error => {
-        console.log('Error');
-      });
+      );
+    }
+  }
+
+  errorInputs(state: boolean) {
+    this.errorUser = state;
+    this.errorPassword = state;
   }
 }
---
+++ src/app/shared/config/configToastError.model.ts
@@ -0,0 +1,8 @@
+export class ToastErrorSettings {
+  public static TOAST_ERROR_SETINGS = {
+    timeOut: 3000,
+    progressBar: true,
+    closeButton: true,
+    positionClass: 'toast-bottom-right'
+  };
+}
